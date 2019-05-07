3b4d360101a8d62bdfb8f7133bb0f04d13c9a88d
Elizabeth Cottrell <elizabeth.cottrell@gmail.com>
2019-05-06 20:38:14 -0400

    update connection

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,13 @@
 var mysql = require("mysql");
 var connection;
 
-if (process.env.JAWSBD_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "root",
-    database: "burgers_db"
-  });
-}
-
+connection = mysql.createConnection({
+  host: "gzp0u91edhmxszwf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  port: 3306,
+  user: "dk879ogz35ovvbxz",
+  password: "ocxv99ft4ihbxvd0",
+  database: "xzbojmzsjm2v7jcw"
+});
 
 connection.connect(function(err) {
   if (err) {
